6065d488ee4915b363eb16bc3f6017db2dc4c080
Rawnly <fedevitale99@gmail.com>
2021-07-09 01:28:17 +0200

    updated wallpaper usage andn download

--- src/lib/utils/photos.rs
+++ src/lib/utils/photos.rs
@@ -2,14 +2,26 @@ use std::io::Result;
 
 use spinners::{Spinner, Spinners};
 
+use wallpaper;
 use crate::api::unsplash::{RandomPhotoParams, Unsplash};
+use crate::lib::utils::download_file;
+use crate::api::models;
 
 pub fn random_photo(api: &Unsplash, params: RandomPhotoParams) -> Result<()> {
     let spinner = Spinner::new(Spinners::Arc, "Loading...".into());
     let photo = api.get_random_photo(params)?;
-    wallpaper::set_from_url(&photo.urls.raw).expect("Wallpaper Error");
 
-    spinner.stop();
+    match download_and_set(photo) {
+        Ok(_) => {
+            spinner.stop();
+            println!("Wallpaper successfully set");
+        },
+        Err(e) => {
+            spinner.stop();
+            eprintln!("An error is occurred: {}", e);
+        }
+    }
+
 
     Ok(())
 }
@@ -18,9 +30,31 @@ pub fn photo_of_the_day(api: &Unsplash) -> Result<()> {
     let spinner = Spinner::new(Spinners::Arc, "Loading...".into());
     let photo = api.get_photo_of_the_day()?;
 
-    wallpaper::set_from_url(&photo.urls.raw).expect("Wallpaper Error"); // TODO: Error handling done well...
+    match download_and_set(photo) {
+        Ok(_) => {
+            spinner.stop();
+            println!("Wallpaper successfully set");
+        },
+        Err(e) => {
+            spinner.stop();
+            eprintln!("An error is occurred: {}", e);
+        }
+    }
 
-    spinner.stop();
 
     Ok(())
+}
+
+
+fn download_and_set(photo: models::Photo) -> std::result::Result<(), Box<::std::error::Error>> {
+    let filepath = match download_file(&photo.urls.raw, &format!("{}.jpg", photo.id)) {
+        Ok(path) => path,
+        Err(e) => {
+            eprintln!("An error is occurred: {}", e);
+            String::new()
+        }
+    };
+
+
+    wallpaper::set_from_path(&filepath)
 }
