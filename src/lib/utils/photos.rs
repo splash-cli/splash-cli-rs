e01bb1f15dd18f3ca2b3efa06a3c8c49397be755
Leonardo Razovic <leorazzo@gmail.com>
2021-07-09 11:30:49 +0200

    refactor: using anyhow to handle results

--- src/lib/utils/photos.rs
+++ src/lib/utils/photos.rs
@@ -1,52 +1,25 @@
-use std::io::Result;
+use anyhow::Result;
 
 use spinners::{Spinner, Spinners};
 
-use wallpaper;
+use crate::api::models::{self, Photo};
 use crate::api::unsplash::{RandomPhotoParams, Unsplash};
 use crate::lib::utils::download_file;
-use crate::api::models;
+use wallpaper;
 
 pub fn random_photo(api: &Unsplash, params: RandomPhotoParams) -> Result<()> {
     let spinner = Spinner::new(Spinners::Arc, "Loading...".into());
     let photo = api.get_random_photo(params)?;
-
-    match download_and_set(photo) {
-        Ok(_) => {
-            spinner.stop();
-            println!("Wallpaper successfully set");
-        },
-        Err(e) => {
-            spinner.stop();
-            eprintln!("An error is occurred: {}", e);
-        }
-    }
-
-
-    Ok(())
+    download_check(photo, spinner)
 }
 
 pub fn photo_of_the_day(api: &Unsplash) -> Result<()> {
     let spinner = Spinner::new(Spinners::Arc, "Loading...".into());
     let photo = api.get_photo_of_the_day()?;
-
-    match download_and_set(photo) {
-        Ok(_) => {
-            spinner.stop();
-            println!("Wallpaper successfully set");
-        },
-        Err(e) => {
-            spinner.stop();
-            eprintln!("An error is occurred: {}", e);
-        }
-    }
-
-
-    Ok(())
+    download_check(photo, spinner)
 }
 
-
-fn download_and_set(photo: models::Photo) -> std::result::Result<(), Box<dyn std::error::Error>> {
+fn download_and_set(photo: models::Photo) -> Result<(), Box<dyn std::error::Error>> {
     let filepath = match download_file(&photo.urls.raw, &format!("{}.jpg", photo.id)) {
         Ok(path) => path,
         Err(e) => {
@@ -55,6 +28,20 @@ fn download_and_set(photo: models::Photo) -> std::result::Result<(), Box<dyn std
         }
     };
 
-
     wallpaper::set_from_path(&filepath)
+}
+
+fn download_check(photo: Photo, spinner: Spinner) -> Result<()> {
+    match download_and_set(photo) {
+        Ok(_) => {
+            spinner.stop();
+            println!("Wallpaper successfully set");
+        }
+        Err(e) => {
+            spinner.stop();
+            eprintln!("An error is occurred: {}", e);
+        }
+    }
+
+    Ok(())
 }
