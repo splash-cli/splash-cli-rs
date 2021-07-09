7bb0e7598d4374763e6bd9036509e7fd7e4a83b0
Leonardo Razovic <leorazzo@gmail.com>
2021-07-09 11:29:05 +0200

    build: using reqwest instead of isahc

--- src/api/unsplash.rs
+++ src/api/unsplash.rs
@@ -1,9 +1,8 @@
-use isahc::prelude::*;
+use crate::api::models::*;
 use serde_json::from_str;
+use reqwest;
 use std::{collections::HashMap, str::FromStr};
 use url::Url;
-
-use crate::api::models::*;
 
 pub type Params = HashMap<&'static str, &'static str>;
 
@@ -20,24 +19,24 @@ impl Unsplash {
         }
     }
 
-    pub fn get_photo(&self, photo_id: &str) -> Result<Photo, isahc::Error> {
+    pub fn get_photo(&self, photo_id: &str) -> Result<Photo, reqwest::Error> {
         let response_text = self.get(&format!("/photos/{}", photo_id), HashMap::new())?;
         let photo: Photo = from_str(&response_text).expect("Error while decoding JSON");
 
         Ok(photo)
     }
 
-    pub fn get_random_photo(&self, params: RandomPhotoParams) -> Result<Photo, isahc::Error> {
+    pub fn get_random_photo(&self, params: RandomPhotoParams) -> Result<Photo, reqwest::Error> {
         let response_text = self.get("/photos/random", params.into_hash_map())?;
         let photo: Photo = from_str(&response_text).expect("Error while decoding JSON");
 
         Ok(photo)
     }
 
-    pub fn get_photo_of_the_day(&self) -> Result<Photo, isahc::Error> {
+    pub fn get_photo_of_the_day(&self) -> Result<Photo, reqwest::Error> {
         let url = Url::parse("https://lambda.splash-cli.app/api").unwrap();
 
-        let mut response = isahc::get(url.as_str())?;
+        let response = reqwest::blocking::get(url.as_str())?;
         let text = response.text()?;
 
         // Parse JSON
@@ -49,7 +48,11 @@ impl Unsplash {
     }
 
     // Private API
-    fn get(&self, path: &str, query_params: HashMap<&str, String>) -> Result<String, isahc::Error> {
+    fn get(
+        &self,
+        path: &str,
+        query_params: HashMap<&str, String>,
+    ) -> Result<String, reqwest::Error> {
         let mut url = self.url.clone();
         url.set_path(path);
 
@@ -58,17 +61,16 @@ impl Unsplash {
                 url.query_pairs_mut().append_pair(key, value.as_str());
             }
         }
+        let client = reqwest::blocking::Client::new();
 
-        let req = isahc::Request::builder()
-            .method("GET")
+        let res = client
+            .get(url)
             .header("Authorization", &format!("Client-ID {}", self.api_key))
-            .uri(url.as_str())
-            .body(())?;
-
-        let mut response = isahc::send(req)?;
-        let text = response.text()?;
-
-        Ok(text)
+            .send()?;
+        match res.text() {
+            Ok(text) => Ok(text),
+            Err(e) => Err(e),
+        }
     }
 }
 pub struct RandomPhotoParams {
